2e869d60e0a9c1ece16986b74f795d8d372360d3
John Jairo Martinez <johnjairo25@gmail.com>
2019-07-06 10:43:17 -0500

    feat: StageToRedshift operator and tasks.

--- dags/migrate_data_dag.py
+++ dags/migrate_data_dag.py
@@ -20,7 +20,7 @@ default_args = {
 dag = DAG('migrate_data_to_redshift',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *',
+          # schedule_interval='0 * * * *',
           catchup=False
           )
 
@@ -28,12 +28,26 @@ start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    file_type="json",
+    json_parameter="s3://udacity-dend/log_json_path.json"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data/A/A/A",
+    file_type="json",
+    json_parameter="auto"
 )
 
 load_songplays_table = LoadFactOperator(
